15edefdec2c28a60f1dee0e767bfd8c0309673d3
frankhn <harfrank2@gmail.com>
2021-01-25 22:12:06 +0200

    web camera

--- webCam.py
+++ webCam.py
@@ -7,5 +7,5 @@ cap.set(4, 480)
 while True:
     success, img = cap.read()
     cv2.imshow("Video", img)
-    if(cv2.waitKey(1) & 0xFF == ord('q')):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
